351ff722a478a8d113dba9a9f28a8b91eea27091
suvorov19 <82819675+suvorov19@users.noreply.github.com>
2021-05-15 14:30:55 +0300

    update Pairs.m with additional information

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,20 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    
+    int count = 0;
+    
+    for (NSInteger i = 0; i < array.count; i++) {
+        
+        for (NSInteger j = 0; j < array.count; j++) {
+            
+            if ([array[i] intValue] - [array[j] intValue] == [number intValue]) {
+                count += 1;
+            }
+        }
+    }
+    
+    return count;
 }
 
 @end
