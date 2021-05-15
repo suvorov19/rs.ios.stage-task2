8cf1815f56988f251b181ed102b9322e449ddd60
suvorov19 <82819675+suvorov19@users.noreply.github.com>
2021-05-15 14:01:24 +0300

    update ReverseBinary.m with additional information

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,29 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    
+    NSMutableArray *reversedbinaryArray = [NSMutableArray array];
+    
+    for (int i = 0; i < 8; i++) {
+        
+        if (n % 2 == 0) {
+            [reversedbinaryArray insertObject:@"0" atIndex:i];
+        }
+        
+        else {
+            [reversedbinaryArray insertObject:@"1" atIndex:i];
+        }
+        
+        n /= 2;
+    }
+    
+    UInt8 reversedDecimalNumber = 0;
+    int multiplier = 1;
+    
+    for (int i = 7; i >= 0; i--) {
+        reversedDecimalNumber += [reversedbinaryArray[i] intValue] * multiplier;
+        multiplier *= 2;
+    }
+    
+    return reversedDecimalNumber;
 }
