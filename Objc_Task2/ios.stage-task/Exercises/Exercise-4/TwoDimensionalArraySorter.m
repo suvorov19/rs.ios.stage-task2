3ad1e48b90a68b89a2a0d78b8e5b89ec96108759
suvorov19 <82819675+suvorov19@users.noreply.github.com>
2021-05-17 14:36:49 +0300

    update TwoDimensionalArraySorter.m with additional information

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,30 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    
+    NSMutableArray *arrayCopy = [NSMutableArray array];
+    NSMutableArray *newArray = [NSMutableArray array];
+    
+    if (![array[0] isKindOfClass:[NSArray class]]) {
+        return @[];
+    }
+    
+    for (int i = 0; i < array.count; i++) {
+        for (int j = 0; j < array[i].count; j++) {
+            
+            [arrayCopy addObject: array[i][j]];
+            
+            if ([arrayCopy[i] isKindOfClass:[NSNumber class]]) {
+                [newArray addObject: array[i][j]];
+            }
+            
+            else if ([arrayCopy[i] isKindOfClass:[NSString class]]) {
+                [newArray addObject: array[i][j]];
+            }
+            
+            [newArray sortUsingSelector:@selector(compare:)];
+        }
+    }
+    return newArray;
 }
-
 @end
